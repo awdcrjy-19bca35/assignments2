5515ea9fda6a8889e5ea39a8dd59daf48b1b8cde
awdcrjy-19bca35 <86473369+awdcrjy-19bca35@users.noreply.github.com>
2021-07-15 23:42:37 +0530

    Update lastprog157.java

--- lastprog157.java
+++ lastprog157.java
@@ -1,3 +1,4 @@
+// java program to print odd mumbers at odd array index and even numbers at even array index // date: 15 july 2021
 import java.util.*;
 import java.lang.*;
 class Main {
@@ -23,7 +24,7 @@ class Main {
                 nums[i]=val;
                 i+=2;
             }
-            System.out.println(nums);
+            
       return nums;
     }
     public static void main(String args[])
@@ -35,6 +36,31 @@ class Main {
         {
             nums[i]=sc.nextInt();
         }
-        sortArrayByParityII(nums);
+        int ans[]=sortArrayByParityII(nums);
+        for(int i=0; i<n ; i++)
+        {
+        System.out.print(" "+ans[i]);
+        }
     } 
 }
+/*-----------------------------------------------------
+output:
+Enter your array size and elements:
+
+4
+
+1 7 4 2
+
+ 4 1 2 7
+ -----------------------------------
+ output:
+ Enter your array size and elements:
+
+6
+
+6 1 5 2 3 4
+
+ 6 1 2 5 4 3
+
+
+
