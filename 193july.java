e3b5995c6bb66bbe2bd68a88e2cc5092e38e175c
awdcrjy-19bca35 <86473369+awdcrjy-19bca35@users.noreply.github.com>
2021-07-19 23:35:57 +0530

    Update 193july.java

--- 193july.java
+++ 193july.java
@@ -15,8 +15,10 @@ public class Main
             map.put(a[i],1);
         }
     }
-    System.out.print(map);
-}
+    System.out.println("Below is the frequency of elements:");
+    System.out.println(map); // in question, it was asked to print all elements and their frequencies
+}                            // but in the example output and output line, only repeated elements frequency was printed
+                             // I have printed all the elements which have repeated and also those which haven't repeated.
 	public static void main(String[] args) {
 	    Scanner sc=new Scanner(System.in);
 	    System.out.println("Enter map size and elements");
@@ -29,12 +31,30 @@ public class Main
 		funct(a,n);
 	}
 }
+
+/*----------------------------------------------------
+output:
+Enter map size and elements
+
+5
+
+1 2 3 3 2
+
+Below is the frequency of elements:
+
+{1=1, 2=2, 3=2}
+
+
+------------------------------------------------
+
 // output
 Enter map size and elements
 
 10
 
 1 2 3 4 1 2 3 4 7 0
+
+Below is the frequency of elements:
 
 {0=1, 1=2, 2=2, 3=2, 4=2, 7=1}
 
