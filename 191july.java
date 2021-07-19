4bdbae89e4526432eb7f8e6378ed4246a1ad6f61
awdcrjy-19bca35 <86473369+awdcrjy-19bca35@users.noreply.github.com>
2021-07-20 00:07:56 +0530

    Create 191july.java

---
+++ 191july.java
@@ -0,0 +1,165 @@
+// java program to perform various hashmap functions / date: 19 july 2021
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+// java program to diaplay menu using linked list // date:16 july 2021
+import java.util.*;
+public class Main
+{
+  public static void main (String[]args)
+  {
+    Scanner sc = new Scanner (System.in);
+    Map <Integer,Integer > map = new HashMap < Integer,Integer >();
+    map.put(1,8);
+    map.put(2,123);
+    map.put(3,64);
+    char cho;
+    do
+    {
+    System.out.println("Enter your choice :");
+	System.out.println(" a.To add an entry with key x and value y to the Hashmap");
+	System.out.println(" b.To print value of x if it is present in Hashmap, else print -1");
+	System.out.println(" c.To print the size of Hashmap");
+	System.out.println(" d.To remove an entry with key x from the Hashmap");
+	System.out.println(" e.To Exit");
+	cho = sc.next().charAt(0);
+	switch (cho)
+	  {
+	  case 'a':
+	       System.out.println ("Enter key and value");
+	    map.put (sc.nextInt(),sc.nextInt());
+	    break;
+	    
+	    case 'b':
+	        System.out.println("Enter search element's key value :");
+	    int k = sc.nextInt ();
+	    if (map.containsKey (k))
+	    {
+	        System.out.println (map.get (k));
+	    }
+	    else
+	    {
+	        System.out.println ("-1");
+	    }
+	    break;
+	    case 'c':
+	        System.out.println ("Size of the map is: "+map.size ());
+	    break;
+	    case 'd':
+	        System.out.println("Enter key value of the element to be removed:");
+	      map.remove (sc.nextInt ());
+	      break;
+	  }
+      }while (cho != 'e');
+}
+}
+
+
+/*--------------------------------------
+output:
+Enter your choice :
+
+ a.To add an entry with key x and value y to the Hashmap
+
+ b.To print value of x if it is present in Hashmap, else print -1
+
+ c.To print the size of Hashmap
+
+ d.To remove an entry with key x from the Hashmap
+
+ e.To Exit
+
+c
+
+Size of the map is: 3
+
+Enter your choice :
+
+ a.To add an entry with key x and value y to the Hashmap
+
+ b.To print value of x if it is present in Hashmap, else print -1
+
+ c.To print the size of Hashmap
+
+ d.To remove an entry with key x from the Hashmap
+
+ e.To Exit
+
+a
+
+Enter key and value
+
+5 10
+
+Enter your choice :
+
+ a.To add an entry with key x and value y to the Hashmap
+
+ b.To print value of x if it is present in Hashmap, else print -1
+
+ c.To print the size of Hashmap
+
+ d.To remove an entry with key x from the Hashmap
+
+ e.To Exit
+
+c
+
+Size of the map is: 4
+
+Enter your choice :
+
+ a.To add an entry with key x and value y to the Hashmap
+
+ b.To print value of x if it is present in Hashmap, else print -1
+
+ c.To print the size of Hashmap
+
+ d.To remove an entry with key x from the Hashmap
+
+ e.To Exit
+
+d
+
+Enter key value of the element to be removed:
+
+3
+
+Enter your choice :
+
+ a.To add an entry with key x and value y to the Hashmap
+
+ b.To print value of x if it is present in Hashmap, else print -1
+
+ c.To print the size of Hashmap
+
+ d.To remove an entry with key x from the Hashmap
+
+ e.To Exit
+
+b
+
+Enter search element's key value :
+
+3
+
+-1
+
+Enter your choice :
+
+ a.To add an entry with key x and value y to the Hashmap
+
+ b.To print value of x if it is present in Hashmap, else print -1
+
+ c.To print the size of Hashmap
+
+ d.To remove an entry with key x from the Hashmap
+
+ e.To Exit
+
+e
