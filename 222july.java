b86e5396b8c95aa61432a6560a7bb2018f942d0c
awdcrjy-19bca35 <86473369+awdcrjy-19bca35@users.noreply.github.com>
2021-07-23 13:59:20 +0530

    Update 222july.java

--- 222july.java
+++ 222july.java
@@ -23,7 +23,53 @@ public class Main
 		System.out.println(ans);
 	}
 }
+/*  using stack methods
+class Solution {
+    public int firstUniqChar(String s) {
+       Stack<Character> sta=new Stack<Character>();
+       Stack<Character> sta1=new Stack<Character>();
+       Stack<Character> sta3=new Stack<Character>();
 
+        sta.push(s.charAt(0));
+        for(int i=1;i<s.length();i++)
+        {
+            while(sta.empty()==false)
+            {
+                if(sta.peek()==s.charAt(i))
+                {   sta3.push(sta.peek());
+                    sta.pop();
+
+                    break;
+                }
+                else
+                {
+                   sta1.push(st.peek());
+                    sta.pop();
+
+                }
+            }
+
+            if(sta3.contains(s.charAt(i))==false)
+            sta.push(s.charAt(i));
+            sta.addAll(sta1);
+            sta1.clear();
+        }
+        if(sta.empty())
+            return -1;
+        int e=sta.peek();
+        sta.pop();
+        while(!sta.empty())
+        {
+        if(s.indexOf(sta.peek())<s.indexOf(e))
+        e=sta.peek();
+        sta.pop();
+        }
+
+
+        return s.indexOf(e);
+    }
+} 
+*/
 
 /*------------------------------------
 output:
