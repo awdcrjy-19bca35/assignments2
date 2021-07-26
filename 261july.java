ffd7905b0ffe7d582f1eb9cb0bb603447e30ef86
awdcrjy-19bca35 <86473369+awdcrjy-19bca35@users.noreply.github.com>
2021-07-26 23:04:42 +0530

    Update 261july.java

--- 261july.java
+++ 261july.java
@@ -1,20 +1,17 @@
 // leetcode program https://leetcode.com/problems/count-primes/  // date: 26 july 2021
 class Solution
-{  
-    public int countPrimes(int n) 
-    {
-        int count=0;
-        for(int i=1;i<n;i++)
-	      {
-	       int c=0;
-	        for(int j=1;j<=i/2;j++)
-	        {
-	         if(i%j==0)
+{public int countPrimes(int n) 
+ { int count=0;
+   for(int i=1;i<n;i++)
+    { int c=0;
+	for(int j=1;j<=i/2;j++)
+	 {
+	   if(i%j==0)
            { c++; }
-	        }
-	        if(c==1)
-	        count++;
-	       }   
+	  }
+	  if(c==1)
+	  count++;
+    }   
       return count;  
     } //method
 } //class
@@ -32,5 +29,5 @@ Time Limit Exceeded
 Details 
 Last executed input
 499979
-Time Submitted    Status              Runtime  Memory   Language
-07/26/2021 21:36	Time Limit Exceeded	N/A	     N/A     	java
+Time Submitted    Status              Runtime       Memory   Language
+07/26/2021 21:36  Time Limit Exceeded	N/A	     N/A     	java
